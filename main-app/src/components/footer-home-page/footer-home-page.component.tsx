498a2f08c2b21470977ed68378eef2f64ee8a01e
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2024-01-05 12:10:47 +0100

    footer refactored

--- main-app/src/components/footer-home-page/footer-home-page.component.tsx
+++ main-app/src/components/footer-home-page/footer-home-page.component.tsx
@@ -13,12 +13,15 @@ const FooterHomePage = () => {
                 <p>Nederland</p>
             </div>
             <div className="footer-home-page__contacts">
-                <p><span><GoMail /></span>office@agro-machines.nl</p>
-                <p><span><HiOutlinePhone /></span> +359 876 962484</p>
-            </div>
-            <div className="footer-home-page__contacts">
                 <p><span><GoLocation /></span>7513EG, Haaksbergersraat 294,</p>
                 <p><span></span>Enschede. Netherland</p>
+                <h2></h2>
+                <p><span><GoLocation /></span>4400, Южна промишлена зона,</p>
+                <p><span></span>Пазарджик, България</p>
+            </div>
+            <div className="footer-home-page__contacts">
+                <p><span><GoMail /></span>office@agro-machines.nl</p>
+                <p><span><HiOutlinePhone /></span> +359 876 962484</p>
             </div>
         </div>
     )
